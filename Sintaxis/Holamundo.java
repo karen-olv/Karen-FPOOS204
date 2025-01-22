dd0761b746d741ab6487444ff977d7a00d5770fb
karen-olv <123047625@upq.edu.mx>
2025-01-22 15:36:26 -0600

    Finalizacion de pratica No.2 Sintaxis

--- Sintaxis/Holamundo.java
+++ Sintaxis/Holamundo.java
@@ -1,7 +1,8 @@
 public class Holamundo {
+    
     public static void main(String[] args) {
+
         System.out.println("Hola, mundo!");
-        
-        System.out.println("Hola, Karen!");
+
     }
 }
---
+++ Sintaxis/Main.java
@@ -0,0 +1,81 @@
+//Importaciones
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        /* 
+
+        System.out.println("Hola, Karen!");
+        String cadenas = "Ivan" + "Isay" + "Guerra"+  "Lopez";
+
+
+         /* 
+           System.out.println(cadenas.lenght());
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(beginIndex:2));
+        System,out.println(cadenas.substring(0,5));
+
+         
+       
+        //variables
+        int x=5,x2;
+double y=1.25,y2;
+String z="204",z1;
+
+x2= Integer.parseInt(z);
+z1= String.valueOf(y);
+double asd = Double.valueOf(x);
+
+System.out.println(x2);
+System.out.println(z1);
+System.out.println(asd);
+
+// conversiones implicitas
+Random rdn = new Random();
+
+int numAleatorio = rdn.nextInt();
+double numDouble = rdn.nextDouble();
+System.out.println("Aleatorio entero:"+numAleatorio);
+System.out.println("Aleatorio double:"+numDouble);
+    
+
+// solicitud de datos
+
+Scanner scn = new Scanner(System.in);
+
+System.out.println("Introduce cualquier dato");
+String dato = scn.nextLine();
+
+System.out.println("Introduce dato entero");
+int datoentero= scn.nextInt();
+
+System.out.println("Introduce dato con decimales");
+double datodecimal = scn.nextDouble();
+
+System.out.println("cualquier dato" + dato);
+System.out.println("Dato entero" + datoentero);
+System.out.println("Dato decimal" + datodecimal);
+
+
+
+ //Boolean operadores logicos y de comparacion
+System.out.println(10 > 9); // true 
+System.out.println(10 == 9); // false
+System.out.println(10 < 9); //false
+System.out.println(10 >= 9); // true 
+System.out.println(10 <= 9); // false 
+System.out.println(10 != 9); //true
+ */
+
+int x = 3;
+System.out.println( x< 5 && x>10);//false
+System.out.println( x< 5 || x>10);
+System.out.println( ! (x< 5 && x>10));
+
+
+    }
+}
