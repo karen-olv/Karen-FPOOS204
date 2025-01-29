745d6d16c1a9582c6ada97c515ac783a415f83c7
karen-olv <123047625@upq.edu.mx>
2025-01-29 15:19:24 -0600

    Practica 5 ejercicio 3
    muestra la tabla de multiplicar del 1 hasta 10 utilizando el for y el system para imprimir.

---
+++ Practica 5/src/Ejercicio3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    
+    public static void main (String[] args ){
+       Scanner scn = new Scanner(System.in);
+
+       for (int i = 1; i <= 10; i++) {
+        System.out.println("Tabla del " + i + ": " + i*1 + " " + i*2 + " " + i*3 + " " + i*4 + " " + i*5 + 
+                           " " + i*6 + " " + i*7 + " " + i*8 + " " + i*9 + " " + i*10);
+       }
+
+    }
+}
